07eb485f3761dc7ebb81a010c25540c60b97df21
Jardel Rodrigues <rodriguinhosilva96@mail.com>
2024-06-27 14:48:28 -0300

    Ajustes

--- src/main/java/com/softstream/despesa/services/MesService.java
+++ src/main/java/com/softstream/despesa/services/MesService.java
@@ -1,8 +1,7 @@
 package com.softstream.despesa.services;
 
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.slf4j.LoggerFactoryFriend;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,20 +10,18 @@ import com.softstream.despesa.entities.Mes;
 import com.softstream.despesa.repositores.MesRepository;
 import com.softstream.despesa.services.exception.ResourceNotFoundException;
 
-
-
 @Service
 public class MesService {
 
 	@Autowired
 	private MesRepository repository;
-	
-//	private static final Logger log = LoggerFactory.getLogger(MesService.class);
+
+	private Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@Transactional(readOnly = true)
 	public Mes findById(Long id) {
+		log.info("ID: " + id);
 		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não Encontrado"));
 	}
-
-
+	
 }
